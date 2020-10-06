3f6d30c9fcc13be49256e86bb02266a892f84408
tana <tankica_ristova@hotmail.com>
2020-10-06 15:53:10 +0200

    Added endpoint for updating tag and fixed /tags/{tagId}/notes response

--- Notes/src/main/resources/data.sql
+++ Notes/src/main/resources/data.sql
@@ -5,7 +5,7 @@ insert into tag (id, name, user_id) values (1,'Tag1',1);
 insert into tag (id, name, user_id) values (2,'Tag2',1);
 insert into tag (id, name, user_id) values (3,'Tag3',2);
 insert into tag (id, name, user_id) values (4,'Tag4',2);
-insert into tag (id, name, user_id) values (5,'Tag5',2);
+insert into tag (id, name, user_id) values (5,'Tag5',1);
 
 insert into note (id, title, content, user_id) values (1,'title1','content1',1);
 insert into note (id, title, content, user_id) values (2,'title2','content2',2);
@@ -14,8 +14,8 @@ insert into note (id, title, content, user_id) values (4,'title4','content4',2);
 insert into note (id, title, content, user_id) values (5,'title5','content5',1);
 
 insert into note_tags (note_id,tags_id) values (1,1);
-insert into note_tags (note_id,tags_id) values (2,1);
-insert into note_tags (note_id,tags_id) values (3,3);
+insert into note_tags (note_id,tags_id) values (1,2);
+insert into note_tags (note_id,tags_id) values (3,2);
 insert into note_tags (note_id,tags_id) values (3,5);
-insert into note_tags (note_id,tags_id) values (4,5);
-insert into note_tags (note_id,tags_id) values (5,5);
+insert into note_tags (note_id,tags_id) values (4,3);
+insert into note_tags (note_id,tags_id) values (4,4);
